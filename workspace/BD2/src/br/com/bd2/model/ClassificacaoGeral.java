7369106e12c860279f9490f6fb7a3730a7a3e641
Rodrigo S. Santos <rodrigoss.br@gmail.com>
2014-08-21 21:25:24 -0300

    primeiro commit

---
+++ workspace/BD2/src/br/com/bd2/model/ClassificacaoGeral.java
@@ -0,0 +1,11 @@
+package br.com.bd2.model;
+
+public class ClassificacaoGeral {
+	private int idClassificacaoGeral;
+	private int idCampeonato;
+	private String posicao;
+	private int idClube;
+	
+	private Campeonato campeonato;
+	private Clube clube;
+}
---
+++ workspace/bd2aula2hibernate/BD2/src/br/com/bd2/hibernate/model/Dependente.java
@@ -0,0 +1,75 @@
+package br.com.bd2.hibernate.model;
+import static javax.persistence.GenerationType.SEQUENCE;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Dependente {
+	@Id
+	@GeneratedValue(strategy = SEQUENCE, generator = "s_dependente")
+	@SequenceGenerator(name="s_dependente", sequenceName="s_dependente")
+	private Long id;
+	
+	@Column(name="nomeDep", nullable=false, length=50)
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name="pessoa")
+	private Pessoa pessoa;
+
+	public Dependente(String nome) { this.nome = nome; }
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dependente other = (Dependente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+}
