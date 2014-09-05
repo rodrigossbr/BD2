26bc8fb871bd3dbe0e49787a964aded56e121777
Rodrigo S. Santos <rodrigoss.br@gmail.com>
2014-09-04 21:41:07 -0300

    Incluidas as libs.

---
+++ workspace/BD2/src/br/com/bd2/main/Main.java
@@ -0,0 +1,35 @@
+package br.com.bd2.main;
+
+import br.com.bd2.entities.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Tecnico tecnico1 = new Tecnico("Tecnico 1");
+		Tecnico tecnico2 = new Tecnico("Tecnico 2");
+		
+		Diretor diretor1 = new Diretor("Diretor 1");
+		Diretor diretor2 = new Diretor("Diretor 2");
+		
+		Clube clube1 = new Clube("Clube 1");
+		Clube clube2 = new Clube("Clube 2");
+		
+		Jogador jogador1 = new Jogador("Jogador 1");
+		Jogador jogador2 = new Jogador("Jogador 2");
+		Jogador jogador3 = new Jogador("Jogador 3");
+		Jogador jogador4 = new Jogador("Jogador 4");
+		Jogador jogador5 = new Jogador("Jogador 5");
+		Jogador jogador6 = new Jogador("Jogador 6");
+		Jogador jogador7 = new Jogador("Jogador 7");
+		Jogador jogador8 = new Jogador("Jogador 8");
+		Jogador jogador9 = new Jogador("Jogador 9");
+		Jogador jogador10 = new Jogador("Jogador 10");
+		Jogador jogador11 = new Jogador("Jogador 11");
+		
+		Campeonato campeonato = new Campeonato("Campeonato 1");
+		campeonato.setAno(2014);
+		
+	}
+}
