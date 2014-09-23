bdf93f2be1a2b6139ac63886b3b259363f816f06
Rodrigo S. Santos <rodrigoss.br@gmail.com>
2014-09-22 21:57:17 -0300

    Json concluido.

--- workspace/Tabalho2/src/br/com/trabalho2/json/Campeonato.js
+++ workspace/Tabalho2/src/br/com/trabalho2/json/Campeonato.js
@@ -35,6 +35,92 @@
 			"nome" : "diretor 2"
 		}
 	},
+	"torcidasOrganizadas" : {
+		"tocidaOrganizada" : {
+			"id" : "1",
+			"nome" : "Torcida 1"
+		},
+		"tocidaOrganizada" : {
+			"id" : "2",
+			"nome" : "Torcida 2"
+		}
+	},
+	"posicoesJogadores" : {
+		"posicao" : {
+			"id" : "1",
+			"nome" : "posicao 1"
+		},
+		"posicao" : {
+			"id" : "2",
+			"nome" : "posicao 2"
+		},
+		"posicao" : {
+			"id" : "3",
+			"nome" : "posicao 3"
+		}
+	},
+	"jogadores" : {
+		"jogador" : {
+			"id" : "1",
+			"nome" : "jogador 1",
+			"posicoes" : {
+				"posicao" : {
+					"id" : "1"
+				}
+			}
+		},
+		"jogador" : {
+			"id" : "2",
+			"nome" : "jogador 2",
+			"posicoes" : {
+				"posicao" : {
+					"id" : "1"
+				}
+			}
+		},
+		"jogador" : {
+			"id" : "3",
+			"nome" : "jogador 3",
+			"posicoes" : {
+				"posicao" : {
+					"id" : "1"
+				},
+				"posicao" : {
+					"id" : "3"
+				}
+			}
+		},
+		"jogador" : {
+			"id" : "4",
+			"nome" : "jogador 4",
+			"posicoes" : {
+				"posicao" : {
+					"id" : "3"
+				}
+			}
+		},
+		"jogador" : {
+			"id" : "5",
+			"nome" : "jogador 5",
+			"posicoes" : {
+				"posicao" : {
+					"id" : "2"
+				}
+			}
+		},
+		"jogador" : {
+			"id" : "6",
+			"nome" : "jogador 6",
+			"posicoes" : {
+				"posicao" : {
+					"id" : "2"
+				}
+				"posicao" : {
+					"id" : "3"
+				}
+			}
+		}
+	},
 	"Clubes" : {
 		"clube" : {
 			"id" : "1",
@@ -59,49 +145,161 @@
 				"diretor" : {
 					"id" : "1"
 				}
+			},
+			"torcidasOrganizadas" : {
+				"torcidaOrganizada" : {
+					"id" : "1"
+				}
+			},
+			"jogadores" : {
+				"jogador" : {
+					"id" : "1"
+				},
+				"jogador" : {
+					"id" : "2"
+				},
+				"jogador" : {
+					"id" : "3"
+				}
 			}
 		},
 		"clube" : {
 			"id" : "2",
-			"nome" : "Clube 2"
+			"nome" : "Clube 2",
+			"tecnicos" : {
+				"tecnico" : {
+					"id": "2"
+				}
+			}
+			"patrocinadores" : {
+				"patrocinador" : {
+					"id" : "2" 
+				}
+			},
+			"diretores" : {
+				"diretor" : {
+					"id" : "2"
+				}
+			},
+			"torcidasOrganizadas" : {
+				"torcidaOrganizada" : {
+					"id" : "2"
+				}
+			},
+			"jogadores" : {
+				"jogador" : {
+					"id" : "4"
+				},
+				"jogador" : {
+					"id" : "5"
+				},
+				"jogador" : {
+					"id" : "6"
+				}
+			}
 		}
 	},
-	"classificacaoGeral" : {
-		"posicao" : "1",
-		"idClube" : "1"
+	"classificacoesGerais" : {
+		"classificacaoGeral" : {
+			"posicao" : "1",
+			"idClube" : "2"
+		},
+		"classificacaoGeral" : {
+			"posicao" : "2",
+			"idClube" : "1"
+		}
 	},
-	"rodada" : {
-		"idArtilheiro" : "1",
-		"totalJogos" : "10",
-		"totalAmarelos" : "10",
-		"totalVermelhos" : "10",
-		"totalGols" : "20",
-		"jogos" : {
-			"jogo" : {
-				"id" : "1",
-				"idClube1" : "1",
-				"idClube2" : "2",
-				"clube1Gols" : "10",
-				"clube2Gols" : "10",
-				"clube1Pontos" : "10",
-				"clube2Pontos" : "10",
-				"Clube1CartoesAmarelos" : "10",
-				"Clube2CartoesAmarelos" : "10",
-				"Clube1CartoesVermelhos" : "10",
-				"Clube1CartoesVermelhos" : "10"
+	"rodadas" : {
+		"rodada" : {
+			"id" : "1"
+			"idArtilheiro" : "4",
+			"totalJogos" : "10",
+			"totalAmarelos" : "10",
+			"totalVermelhos" : "10",
+			"totalGols" : "20",
+			"jogos" : {
+				"jogo" : {
+					"id" : "1",
+					"idClube1" : "1",
+					"idClube2" : "2",
+					"clube1Gols" : "10",
+					"clube2Gols" : "10",
+					"clube1Pontos" : "10",
+					"clube2Pontos" : "10",
+					"Clube1CartoesAmarelos" : "10",
+					"Clube2CartoesAmarelos" : "10",
+					"Clube1CartoesVermelhos" : "10",
+					"Clube1CartoesVermelhos" : "10"
+				},
+				"jogo" : {
+					"id" : "2"
+					"idClube1" : "1",
+					"idClube2" : "2",
+					"clube1Gols" : "10",
+					"clube2Gols" : "10",
+					"clube1Pontos" : "10",
+					"clube2Pontos" : "10",
+					"Clube1CartoesAmarelos" : "10",
+					"Clube2CartoesAmarelos" : "10",
+					"Clube1CartoesVermelhos" : "10",
+					"Clube1CartoesVermelhos" : "10"
+				}
 			},
-			"jogo" : {
-				"id" : "2"
-				"idClube1" : "1",
-				"idClube2" : "2",
-				"clube1Gols" : "10",
-				"clube2Gols" : "10",
-				"clube1Pontos" : "10",
-				"clube2Pontos" : "10",
-				"Clube1CartoesAmarelos" : "10",
-				"Clube2CartoesAmarelos" : "10",
-				"Clube1CartoesVermelhos" : "10",
-				"Clube1CartoesVermelhos" : "10"
+			"classificacoesRodada" : {
+				"classificacaoRodada" : {
+					"idClube" : "1",
+					"posicao" : "1"
+				},
+				"classificacaoRodada" : {
+					"idClube" : "2",
+					"posicao" : "2"
+				}
+			}
+		},
+		"rodada" : {
+			"id" : "2"
+			"idArtilheiro" : "3",
+			"totalJogos" : "6",
+			"totalAmarelos" : "2",
+			"totalVermelhos" : "1",
+			"totalGols" : "20",
+			"jogos" : {
+				"jogo" : {
+					"id" : "1",
+					"idClube1" : "1",
+					"idClube2" : "2",
+					"clube1Gols" : "10",
+					"clube2Gols" : "10",
+					"clube1Pontos" : "10",
+					"clube2Pontos" : "10",
+					"Clube1CartoesAmarelos" : "10",
+					"Clube2CartoesAmarelos" : "10",
+					"Clube1CartoesVermelhos" : "10",
+					"Clube1CartoesVermelhos" : "10"
+				},
+				"jogo" : {
+					"id" : "2"
+					"idClube1" : "1",
+					"idClube2" : "2",
+					"clube1Gols" : "10",
+					"clube2Gols" : "10",
+					"clube1Pontos" : "10",
+					"clube2Pontos" : "10",
+					"Clube1CartoesAmarelos" : "10",
+					"Clube2CartoesAmarelos" : "10",
+					"Clube1CartoesVermelhos" : "10",
+					"Clube1CartoesVermelhos" : "10"
+				}
+			},
+			"classificacoesRodada" : {
+				"classificacaoRodada" : {
+					"idClube" : "1",
+					"posicao" : "2"
+				},
+				"classificacaoRodada" : {
+					"idClube" : "2",
+					"posicao" : "1"
+				}
 			}
 		}
 	}
