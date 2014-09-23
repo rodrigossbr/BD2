d78484a59a7f0f5c14ba049f68a6d2c84299090a
Rodrigo S. Santos <rodrigoss.br@gmail.com>
2014-09-22 21:16:04 -0300

    Adicionado Json.

---
+++ workspace/Tabalho2/src/br/com/trabalho2/json/Campeonato.js
@@ -0,0 +1,108 @@
+
+
+"Campeonato" :{ 
+	"id" : "1",
+	"nome" : "Brasileiro",
+	"ano" : "2014",
+	"nroClubes" : "2",
+	"tecnicos" : {
+		"tecnico" : {
+			"id" : "1",
+			"nome" : "Tecnico 1"
+		},
+		"tecnico" : {
+			"id" : "2",
+			"nome" : "Tecnico 2"
+		}
+	},
+	"patrocinadores" : {
+		"patrocinador" : {
+			"id" : "1",
+			"nome" : "patrocinador 1"
+		},
+		"patrocinador" : {
+			"id" : "2",
+			"nome" : "patrocinador 2"
+		}
+	},
+	"diretores" : {
+		"diretor" : {
+			"id" : "1",
+			"nome" : "diretor 1"
+		},
+		"diretor" : {
+			"id" : "2",
+			"nome" : "diretor 2"
+		}
+	},
+	"Clubes" : {
+		"clube" : {
+			"id" : "1",
+			"nome" : "Clube 1",
+			"tecnicos" : {
+				"tecnico" : {
+					"id": "1"
+				},
+				"tecnico" : {
+					"id": "2"
+				}
+			}
+			"patrocinadores" : {
+				"patrocinador" : {
+					"id" : "1" 
+				},
+				"patrocinador" : {
+					"id" : "2" 
+				}
+			},
+			"diretores" : {
+				"diretor" : {
+					"id" : "1"
+				}
+			}
+		},
+		"clube" : {
+			"id" : "2",
+			"nome" : "Clube 2"
+		}
+	},
+	"classificacaoGeral" : {
+		"posicao" : "1",
+		"idClube" : "1"
+	},
+	"rodada" : {
+		"idArtilheiro" : "1",
+		"totalJogos" : "10",
+		"totalAmarelos" : "10",
+		"totalVermelhos" : "10",
+		"totalGols" : "20",
+		"jogos" : {
+			"jogo" : {
+				"id" : "1",
+				"idClube1" : "1",
+				"idClube2" : "2",
+				"clube1Gols" : "10",
+				"clube2Gols" : "10",
+				"clube1Pontos" : "10",
+				"clube2Pontos" : "10",
+				"Clube1CartoesAmarelos" : "10",
+				"Clube2CartoesAmarelos" : "10",
+				"Clube1CartoesVermelhos" : "10",
+				"Clube1CartoesVermelhos" : "10"
+			},
+			"jogo" : {
+				"id" : "2"
+				"idClube1" : "1",
+				"idClube2" : "2",
+				"clube1Gols" : "10",
+				"clube2Gols" : "10",
+				"clube1Pontos" : "10",
+				"clube2Pontos" : "10",
+				"Clube1CartoesAmarelos" : "10",
+				"Clube2CartoesAmarelos" : "10",
+				"Clube1CartoesVermelhos" : "10",
+				"Clube1CartoesVermelhos" : "10"
+			}
+		}
+	}
+}
