5c1403dafd1d5febcc5a5cd2153dc608ac8e848c
Luciano <luciano@centri.mobi>
2014-09-24 21:04:47 -0300

    Arquivos de xsd, xml e js atualizados.

--- workspace/Tabalho2/src/br/com/trabalho2/json/Campeonato.js
+++ workspace/Tabalho2/src/br/com/trabalho2/json/Campeonato.js
@@ -1,306 +1,222 @@
-
-
-"Campeonato" :{ 
-	"id" : "1",
-	"nome" : "Brasileiro",
-	"ano" : "2014",
-	"nroClubes" : "2",
-	"tecnicos" : {
-		"tecnico" : {
-			"id" : "1",
-			"nome" : "Tecnico 1"
-		},
-		"tecnico" : {
-			"id" : "2",
-			"nome" : "Tecnico 2"
-		}
-	},
-	"patrocinadores" : {
-		"patrocinador" : {
-			"id" : "1",
-			"nome" : "patrocinador 1"
-		},
-		"patrocinador" : {
-			"id" : "2",
-			"nome" : "patrocinador 2"
-		}
-	},
-	"diretores" : {
-		"diretor" : {
-			"id" : "1",
-			"nome" : "diretor 1"
-		},
-		"diretor" : {
-			"id" : "2",
-			"nome" : "diretor 2"
-		}
-	},
-	"torcidasOrganizadas" : {
-		"tocidaOrganizada" : {
-			"id" : "1",
-			"nome" : "Torcida 1"
-		},
-		"tocidaOrganizada" : {
-			"id" : "2",
-			"nome" : "Torcida 2"
-		}
-	},
-	"posicoesJogadores" : {
-		"posicao" : {
-			"id" : "1",
-			"nome" : "posicao 1"
-		},
-		"posicao" : {
-			"id" : "2",
-			"nome" : "posicao 2"
-		},
-		"posicao" : {
-			"id" : "3",
-			"nome" : "posicao 3"
-		}
-	},
-	"jogadores" : {
-		"jogador" : {
-			"id" : "1",
-			"nome" : "jogador 1",
-			"posicoes" : {
-				"posicao" : {
-					"id" : "1"
-				}
-			}
-		},
-		"jogador" : {
-			"id" : "2",
-			"nome" : "jogador 2",
-			"posicoes" : {
-				"posicao" : {
-					"id" : "1"
-				}
-			}
-		},
-		"jogador" : {
-			"id" : "3",
-			"nome" : "jogador 3",
-			"posicoes" : {
-				"posicao" : {
-					"id" : "1"
-				},
-				"posicao" : {
-					"id" : "3"
-				}
-			}
-		},
-		"jogador" : {
-			"id" : "4",
-			"nome" : "jogador 4",
-			"posicoes" : {
-				"posicao" : {
-					"id" : "3"
-				}
-			}
-		},
-		"jogador" : {
-			"id" : "5",
-			"nome" : "jogador 5",
-			"posicoes" : {
-				"posicao" : {
-					"id" : "2"
-				}
-			}
-		},
-		"jogador" : {
-			"id" : "6",
-			"nome" : "jogador 6",
-			"posicoes" : {
-				"posicao" : {
-					"id" : "2"
-				}
-				"posicao" : {
-					"id" : "3"
-				}
-			}
-		}
-	},
-	"Clubes" : {
-		"clube" : {
-			"id" : "1",
-			"nome" : "Clube 1",
-			"tecnicos" : {
-				"tecnico" : {
-					"id": "1"
-				},
-				"tecnico" : {
-					"id": "2"
-				}
-			}
-			"patrocinadores" : {
-				"patrocinador" : {
-					"id" : "1" 
-				},
-				"patrocinador" : {
-					"id" : "2" 
-				}
-			},
-			"diretores" : {
-				"diretor" : {
-					"id" : "1"
-				}
-			},
-			"torcidasOrganizadas" : {
-				"torcidaOrganizada" : {
-					"id" : "1"
-				}
-			},
-			"jogadores" : {
-				"jogador" : {
-					"id" : "1"
-				},
-				"jogador" : {
-					"id" : "2"
-				},
-				"jogador" : {
-					"id" : "3"
-				}
-			}
-		},
-		"clube" : {
-			"id" : "2",
-			"nome" : "Clube 2",
-			"tecnicos" : {
-				"tecnico" : {
-					"id": "2"
-				}
-			}
-			"patrocinadores" : {
-				"patrocinador" : {
-					"id" : "2" 
-				}
-			},
-			"diretores" : {
-				"diretor" : {
-					"id" : "2"
-				}
-			},
-			"torcidasOrganizadas" : {
-				"torcidaOrganizada" : {
-					"id" : "2"
-				}
-			},
-			"jogadores" : {
-				"jogador" : {
-					"id" : "4"
-				},
-				"jogador" : {
-					"id" : "5"
-				},
-				"jogador" : {
-					"id" : "6"
-				}
-			}
-		}
-	},
-	"classificacoesGerais" : {
-		"classificacaoGeral" : {
-			"posicao" : "1",
-			"idClube" : "2"
-		},
-		"classificacaoGeral" : {
-			"posicao" : "2",
-			"idClube" : "1"
-		}
-	},
-	"rodadas" : {
-		"rodada" : {
-			"id" : "1"
-			"idArtilheiro" : "4",
-			"totalJogos" : "10",
-			"totalAmarelos" : "10",
-			"totalVermelhos" : "10",
-			"totalGols" : "20",
-			"jogos" : {
-				"jogo" : {
-					"id" : "1",
-					"idClube1" : "1",
-					"idClube2" : "2",
-					"clube1Gols" : "10",
-					"clube2Gols" : "10",
-					"clube1Pontos" : "10",
-					"clube2Pontos" : "10",
-					"Clube1CartoesAmarelos" : "10",
-					"Clube2CartoesAmarelos" : "10",
-					"Clube1CartoesVermelhos" : "10",
-					"Clube1CartoesVermelhos" : "10"
-				},
-				"jogo" : {
-					"id" : "2"
-					"idClube1" : "1",
-					"idClube2" : "2",
-					"clube1Gols" : "10",
-					"clube2Gols" : "10",
-					"clube1Pontos" : "10",
-					"clube2Pontos" : "10",
-					"Clube1CartoesAmarelos" : "10",
-					"Clube2CartoesAmarelos" : "10",
-					"Clube1CartoesVermelhos" : "10",
-					"Clube1CartoesVermelhos" : "10"
-				}
-			},
-			"classificacoesRodada" : {
-				"classificacaoRodada" : {
-					"idClube" : "1",
-					"posicao" : "1"
-				},
-				"classificacaoRodada" : {
-					"idClube" : "2",
-					"posicao" : "2"
-				}
-			}
-		},
-		"rodada" : {
-			"id" : "2"
-			"idArtilheiro" : "3",
-			"totalJogos" : "6",
-			"totalAmarelos" : "2",
-			"totalVermelhos" : "1",
-			"totalGols" : "20",
-			"jogos" : {
-				"jogo" : {
-					"id" : "1",
-					"idClube1" : "1",
-					"idClube2" : "2",
-					"clube1Gols" : "10",
-					"clube2Gols" : "10",
-					"clube1Pontos" : "10",
-					"clube2Pontos" : "10",
-					"Clube1CartoesAmarelos" : "10",
-					"Clube2CartoesAmarelos" : "10",
-					"Clube1CartoesVermelhos" : "10",
-					"Clube1CartoesVermelhos" : "10"
-				},
-				"jogo" : {
-					"id" : "2"
-					"idClube1" : "1",
-					"idClube2" : "2",
-					"clube1Gols" : "10",
-					"clube2Gols" : "10",
-					"clube1Pontos" : "10",
-					"clube2Pontos" : "10",
-					"Clube1CartoesAmarelos" : "10",
-					"Clube2CartoesAmarelos" : "10",
-					"Clube1CartoesVermelhos" : "10",
-					"Clube1CartoesVermelhos" : "10"
-				}
-			},
-			"classificacoesRodada" : {
-				"classificacaoRodada" : {
-					"idClube" : "1",
-					"posicao" : "2"
-				},
-				"classificacaoRodada" : {
-					"idClube" : "2",
-					"posicao" : "1"
-				}
-			}
-		}
-	}
+{
+  "Campeonato": {
+    "id": "1",
+    "nome": "Campeonato Brasileiro",
+    "ano": "2014",
+    "nroClubes": "2",
+    "ClassificacoesGerais": {
+      "ClassificacaoGeral": [
+        {
+          "id": "1",
+          "posicao": "1",
+          "idClube": "1"
+        },
+        {
+          "id": "2",
+          "posicao": "2",
+          "idClube": "2"
+        }
+      ]
+    },
+    "Rodadas": {
+      "Rodada": {
+        "id": "1",
+        "totalJogos": "2",
+        "totalAmarelos": "1",
+        "totalVermelhos": "2",
+        "totalGols": "4",
+        "jogadorArtilheiro": {
+          "Jogador": {
+            "id": "1",
+            "nome": "João",
+            "posicoes": {
+              "Posicao": {
+                "id": "1",
+                "nome": "Atacante"
+              }
+            }
+          }
+        },
+        "Jogos": {
+          "Jogo": {
+            "id": "1",
+            "Clubes": {
+              "Clube": [
+                {
+                  "id": "1",
+                  "nome": "Inter",
+                  "diretores": {
+                    "Diretor": [
+                      {
+                        "id": "1",
+                        "nome": "Adao"
+                      },
+                      {
+                        "id": "2",
+                        "nome": "Evandro"
+                      }
+                    ]
+                  },
+                  "tecnicos": {
+                    "Tecnico": {
+                      "id": "1",
+                      "nome": "João Alfredo"
+                    }
+                  },
+                  "patrocinadores": {
+                    "Patrocinador": [
+                      {
+                        "id": "1",
+                        "nome": "Nike"
+                      },
+                      {
+                        "id": "1",
+                        "nome": "BBB"
+                      }
+                    ]
+                  },
+                  "torcidasOrganizadas": {
+                    "TorcidaOrganizada": {
+                      "id": "1",
+                      "nome": "Gurias"
+                    }
+                  },
+                  "jogadores": {
+                    "Jogador": [
+                      {
+                        "id": "1",
+                        "nome": "Neimar",
+                        "posicoes": {
+                          "Posicao": {
+                            "id": "1",
+                            "nome": "Atacante"
+                          }
+                        }
+                      },
+                      {
+                        "id": "2",
+                        "nome": "Dida",
+                        "posicoes": {
+                          "Posicao": {
+                            "id": "1",
+                            "nome": "Goleiro"
+                          }
+                        }
+                      },
+                      {
+                        "id": "3",
+                        "nome": "Pelé",
+                        "posicoes": {
+                          "Posicao": {
+                            "id": "1",
+                            "nome": "Atacante"
+                          }
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  "id": "2",
+                  "nome": "Gremio",
+                  "diretores": {
+                    "Diretor": [
+                      {
+                        "id": "1",
+                        "nome": "Adao"
+                      },
+                      {
+                        "id": "2",
+                        "nome": "Evandro"
+                      }
+                    ]
+                  },
+                  "tecnicos": {
+                    "Tecnico": {
+                      "id": "1",
+                      "nome": "João Alfredo"
+                    }
+                  },
+                  "patrocinadores": {
+                    "Patrocinador": [
+                      {
+                        "id": "1",
+                        "nome": "Nike"
+                      },
+                      {
+                        "id": "1",
+                        "nome": "BBB"
+                      }
+                    ]
+                  },
+                  "torcidasOrganizadas": {
+                    "TorcidaOrganizada": {
+                      "id": "1",
+                      "nome": "Gurias"
+                    }
+                  },
+                  "jogadores": {
+                    "Jogador": [
+                      {
+                        "id": "1",
+                        "nome": "Neimar",
+                        "posicoes": {
+                          "Posicao": {
+                            "id": "1",
+                            "nome": "Atacante"
+                          }
+                        }
+                      },
+                      {
+                        "id": "2",
+                        "nome": "Dida",
+                        "posicoes": {
+                          "Posicao": {
+                            "id": "1",
+                            "nome": "Goleiro"
+                          }
+                        }
+                      },
+                      {
+                        "id": "3",
+                        "nome": "Pelé",
+                        "posicoes": {
+                          "Posicao": {
+                            "id": "1",
+                            "nome": "Atacante"
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }
+              ],
+              "clube1Gols": "4",
+              "clube2Gols": "0",
+              "clube1Pontos": "8",
+              "clube2Pontos": "0",
+              "clube1CartaoAmarelo": "0",
+              "clube2CartaoAmarelo": "1",
+              "clube1CartaoVermelho": "0",
+              "clube2CartaoVermelho": "2"
+            }
+          }
+        },
+        "ClassificacoesRodada": {
+          "ClassificacaoRodada": [
+            {
+              "id": "1",
+              "posicao": "1",
+              "idClube": "1"
+            },
+            {
+              "id": "2",
+              "posicao": "2",
+              "idClube": "2"
+            }
+          ]
+        }
+      }
+    }
+  }
 }
